1ce9fdc2867f4f4145cf40e32e62e5882aac8858
alireza talebizadeh <btc.lider78@gmail.com>
2022-09-24 11:08:53 +0330

    shabihe sazi site keycode.info

---
+++ script.js
@@ -0,0 +1,36 @@
+let $ = document;
+
+let locationKey = $.querySelector("#location");
+let title = $.querySelector("#title");
+let keyCodeElem = $.querySelector("#keyCode");
+let keyElem = $.querySelector("#key");
+let locationElem = $.querySelector("#location");
+let whichElem = $.querySelector("#which");
+let codeElem = $.querySelector("#code");
+
+
+document.body.addEventListener("keydown", function (event) {
+    event.preventDefault();
+    starter.style.display = "none";
+    heading.style.display = "flex";
+    ascii.style.display = "flex";
+    infos.style.display = "flex";
+
+    let eventKeyCode = event.keyCode;
+    let eventKey = event.key;
+    let eventLocation = event.location;
+    let eventWhich = event.which;
+    let eventCode = event.code;
+
+    keyCodeElem.innerHTML = eventKeyCode;
+    keyElem.innerHTML = eventKey;
+    locationElem.innerHTML = eventLocation;
+    whichElem.innerHTML = eventWhich;
+    codeElem.innerHTML = eventCode;
+
+    console.log(event);
+})
+
+
+
+
